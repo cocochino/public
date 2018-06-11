2c0b086a2419c8cc50aab911d7ffd68f6bc106fc
m.sjoquist <cocochino@gmail.com>
2018-06-11 14:10:04 -0400

    renamed:    fib_generator.py -> fib_gen.py 	new file:   test/__init__.py 	new file:   test/unit/__init__.py 	delete file:   test/unit/fib_gen_test.py

--- tansu/fib_gen.py
+++ tansu/fib_gen.py
@@ -1,7 +1,14 @@
-class FindFibWithGenerator():
+#/mooshu/tansu/fib_gen.py
+
+
+USER_PROMPT1 = 'Enter Fibonacci sequence numbers (integer between 1 to 1000): '
+USER_PROMPT2 = 'Input must be between 1 to 1000. '
+
+class FindFibWithGenerator:
     """
     Goal: Find Fibonnaci sequence to the nth that is specified by users.
-    It demonstrate Python's Generator
+    It demonstrates Python's Generator.
+    It also is a base for Python unit test experiment.
     """
 
     def get_fib_seq(self, depth=1):
@@ -21,11 +28,15 @@ class FindFibWithGenerator():
         :return: Depth as an Integer.
         """
         while True:
+            #
+            selection = input(USER_PROMPT1)
+            print(selection)
             try:
-                depth = int(input("Enter Fibonacci sequence numbers (integer between 1 to 1000): "))
+                depth = int(selection)
                 if depth < 1 or depth > 1000:
-                    print("Input must be between 1 to 1000.")
+                    print(USER_PROMPT2)
                 else:
+                    print("Calculate {} sequences.".format(depth))
                     break
             except:
                 print("Error: Could not process user input. Please enter integer.")
---
+++ tansu/test/unit/fib_gen_test.py
@@ -0,0 +1,48 @@
+from unittest import TestCase
+from unittest.mock import patch
+from tansu.fib_gen import FindFibWithGenerator as fb
+
+class FibGenTest3(TestCase):
+
+    def test_get_depth_prompt(self):
+        """ Test: Checks that correct prompt is called .
+        This test uses Mock input.
+        """
+        with patch('builtins.input') as mocked_prompt:
+            fb().get_depth()
+            mocked_prompt.assert_called_with('Enter Fibonacci sequence numbers (integer between 1 to 1000): ')
+
+    def test_get_fib_seq_one(self):
+        """ Test: The sequence output should start from 1 """
+        expected = [1]
+        the_seq = []
+        for num in fb().get_fib_seq(1):
+            the_seq.append(num)
+        self.assertEqual(expected, the_seq)
+
+    def test_get_fib_seq_one_negative(self):
+        """ Test: The sequence should not start from 0 """
+        expected = [0]
+        the_seq = []
+        for num in fb().get_fib_seq(1):
+            the_seq.append(num)
+        self.assertNotEqual(expected, the_seq)
+
+    def test_get_fib_seq_twenty(self):
+        """ Test: The first 21 sequences, sanity check. """
+        expected = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765]
+        the_seq = []
+        for num in fb().get_fib_seq(20): # Note that this uses fb()
+            the_seq.append(num)
+        self.assertEqual(expected, the_seq)
+
+
+
+"""
+    def test_get_depth(self):
+         Test: Response to users with correct input number. This uses Mock.
+        with patch('builtins.print') as mocked_print:
+            fb().get_depth(999)
+            mocked_print.assert_called_with('Calculate 999 sequences.')
+
+"""
